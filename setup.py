fcd11ed974589c8008c42dc8a6a59f8e6f6e4889
Arijit Basu <sayanarijit@gmail.com>
2021-02-09 16:48:00 +0530

    Move patched kombu to niteoweb/kombu3

--- setup.py
+++ setup.py
@@ -141,7 +141,7 @@ def reqs(*f):
                 os.path.join(os.getcwd(), 'requirements', *f)).readlines()
         ) if r]
 
-install_requires = ["pytz>dev", "billiard>=3.3.0.23,<3.4", "kombu @ git+https://github.com/sayanarijit/kombu.git@v3"]
+install_requires = ["pytz>dev", "billiard>=3.3.0.23,<3.4", "kombu @ git+https://github.com/niteoweb/kombu3.git@v3"]
 if JYTHON:
     install_requires.extend(reqs('jython.txt'))
 
